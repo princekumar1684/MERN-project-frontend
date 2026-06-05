fed5b181d946a9616df7da217f79e61154fff275
Prince <160013199+princekumar1684@users.noreply.github.com>
2026-06-05 17:57:35 +0530

    final commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,6 @@ const Sidebar = ({ openSidebar, setOpenSidebar }) => {
         { withCredentials: true },
       );
 
-      localStorage.removeItem("token");
       navigate("/login");
     } catch (err) {
       console.log(err);
